4d394d929a2264f90b619e852ce5a5e7924f8f0b
Andrea Hohnsen <a.hohnsen@mail.de>
2022-03-19 14:23:09 +0100

    Connect Frontend with Backend

--- api/users.mjs
+++ api/users.mjs
@@ -7,9 +7,37 @@ export default async function handler(request, response) {
   const { method } = request;
 
   if (method === "GET") {
-    response.json(await User.find());
+    try {
+      const users = await User.find();
+      response.json(users);
+    } catch (error) {
+      response.status(500).json({ message: error.message });
+    }
   } else if (method === "POST") {
-    const user = await User.create(request.body);
-    response.json(user);
+    const user = new User(request.body);
+    try {
+      const newUser = await user.save();
+      response.status(201).json(newUser);
+    } catch (error) {
+      response.status(400).json({ message: error.message });
+    }
+  } else if (method === "PUT") {
+    const { user, _id } = request.body;
+    try {
+      const updatedUser = await User.findByIdAndUpdate(_id, user, {
+        returnOriginal: false,
+      });
+      response.json(updatedUser);
+    } catch (error) {
+      response.status(400).json({ message: error.message });
+    }
+  } else if (method === "DELETE") {
+    const { _id } = request.body;
+    try {
+      const deletedUser = await User.findByIdAndDelete(_id);
+      response.json(deletedUser);
+    } catch (error) {
+      response.status(500).json({ message: error.message });
+    }
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import { useState } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: "" });
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  async function getUsers() {
+    try {
+      const response = await axios.get("/api/users");
+      setUserData(response.data);
+    } catch (error) {
+      console.log("Error:", error.message);
+    }
+    console.log(userData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form onSubmit={handleSubmit((data) => onSubmit(data))}>
+        <Label htmlFor="destination">Name</Label>
+        <Input {...register("name")} id="name" />
+        <Label htmlFor="notes">Email</Label>
+        <Input {...register("email")} id="email" />
+        <button type="submit">Add</button>
+        <button>update</button>
+        <ErrorMessage>
+          <p>{errors.destination && errors.destination.message}</p>
+          <p>{errors.notes && errors.notes.message}</p>
+        </ErrorMessage>
+      </Form>
+
+      {userData &&
+        userData.map((user) => (
+          <div key={user._id}>
+            <p>{user.username}</p>
+            <p>{user.email}</p>
+            <button
+              onClick={() => {
+                handleDelete(user._id);
+              }}
+            >
+              delete
+            </button>
+            <button onClick={() => handleEdit(user._id)}>edit</button>
+          </div>
+        ))}
     </div>
   );
+
+  async function onSubmit(data) {
+    const newUser = { username: data.name, email: data.email };
+    try {
+      await axios.post("/api/users", newUser);
+      getUsers();
+    } catch (error) {
+      console.log("Error", error.messages);
+    }
+  }
+
+  async function handleDelete(_id) {
+    console.log(_id);
+    try {
+      await axios.delete("/api/users/", { data: { _id: _id } });
+      getUsers();
+    } catch (error) {
+      console.log("Error:", error.message);
+    }
+  }
+
+  async function handleEdit(_id) {
+    const data = {
+      _id: _id,
+      user: {
+        username: "Eli",
+        email: "dsfsdfsdf",
+      },
+    };
+    try {
+      await axios.put("/api/users/", data);
+      getUsers();
+    } catch (error) {
+      console.log("Error:", error.message);
+    }
+  }
 }
 
 export default App;
+
+const Form = styled.form`
+  display: grid;
+  width: 300px;
+  gap: 10px;
+  padding: 20px;
+  background-color: #000000;
+`;
+
+const Label = styled.label`
+  color: #ffffff;
+  font-weight: 500;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 5px;
+  border: 0;
+`;
+
+const ErrorMessage = styled.div`
+  text-align: center;
+  color: red;
+  font-size: 0.8rem;
+`;
